ac1f1a7a1297d7f646315272c5eea50194e3ee41
Pisarenko Dmitriy <pdp21u515@student.bmstu.ru>
2023-11-17 01:53:19 +0300

    update lab01

--- lab_01/algorithms.py
+++ lab_01/algorithms.py
@@ -15,7 +15,7 @@ def levenshtein_distance(str1, str2):
                 
             matrix[i][j] = min(action_add, action_delete, action_change)
     
-    print("\МАТРИЦА:")
+    print("\nМАТРИЦА:")
     print_matrix(matrix, str1, str2)
 
     result = matrix[n][m]
@@ -37,12 +37,12 @@ def damerau_levenshtein_distance(str1, str2):
                 action_change += 1
                 
             action_swap = n
-            if (i > 1 and j > 1 and str1[i - 1] == str2[j - 2] and str1[i - 2] == str2[j - 1]):
+            if i > 1 and j > 1 and str1[i - 1] == str2[j - 2] and str1[i - 2] == str2[j - 1]:
                 action_swap = matrix[i - 2][j - 2] + 1
                 
             matrix[i][j] = min(action_add, action_delete, action_change, action_swap)
     
-    print("\МАТРИЦА:")
+    print("\nМАТРИЦА:")
     print_matrix(matrix, str1, str2)
     
     result = matrix[n][m]
@@ -51,10 +51,66 @@ def damerau_levenshtein_distance(str1, str2):
 
 
 def damerau_levenshtein_distance_recursive(str1, str2):
-    return 0
+    n, m = len(str1), len(str2)
+
+    flag = 0
+    if n == 0 or m == 0:
+        return abs(n - m)
+
+    if str1[-1] != str2[-1]:
+        flag = 1
+
+    result = min(
+        damerau_levenshtein_distance_recursive(str1[:-1], str2) + 1,
+        damerau_levenshtein_distance_recursive(str1, str2[:-1]) + 1,
+        damerau_levenshtein_distance_recursive(str1[:-1], str2[:-1]) + flag)
+    
+    if n > 1 and m > 1 and str1[-1] == str2[-2] and str1[-2] == str2[-1]:
+        result = min(result, damerau_levenshtein_distance_recursive(str1[:-2], str2[:-2]) + 1)
+
+    return result
 
 
 def damerau_levenshtein_distance_recursive_cache(str1, str2):
-    return 0
+    n, m = len(str1), len(str2)
+    matrix = create_levenshtein_matrix(n + 1, m + 1)
+
+    for i in range(n + 1):
+        for j in range(m + 1):
+            matrix[i][j] = -1
+
+    recursive_cache(str1, str2, n, m, matrix)
+
+    print("\nМАТРИЦА:")
+    print_matrix(matrix, str1, str2)
+
+    result = matrix[n][m]
+
+    return result
 
 
+def recursive_cache(str1, str2, n, m, matrix):
+    if matrix[n][m] != -1:
+        return matrix[n][m]
+    
+    if n == 0:
+        matrix[n][m] = m
+        return matrix[n][m]
+    
+    if n > 0 and m == 0:
+        matrix[n][m] = n
+        return matrix[n][m]
+
+    action_add = recursive_cache(str1, str2, n - 1, m, matrix) + 1
+    action_delete = recursive_cache(str1, str2, n, m - 1, matrix) + 1
+    action_change = recursive_cache(str1, str2, n - 1, m - 1, matrix)
+
+    if str1[n - 1] != str2[m - 1]:
+        action_change += 1 
+
+    matrix[n][m] = min(action_add, action_delete, action_change)
+
+    if n > 1 and m > 1 and str1[n - 1] == str2[m - 2] and str1[n - 2] == str2[m - 1]:
+        matrix[n][m] = min(matrix[n][m], recursive_cache(str1, str2, n - 2, m - 2, matrix) + 1)
+
+    return matrix[n][m]
--- lab_01/menu.py
+++ lab_01/menu.py
@@ -3,14 +3,14 @@ from algorithms import *
 from tests import *
 
 def description():
-    print("\n\nМЕНЮ: \n\
-1. Расстояние Левенштейна \n\
-2. Расстояние Дамерау-Левенштейна \n\
-3. Расстояние Дамерау-Левенштейна (рекурсивно) \n\
-4. Расстояние Дамерау-Левенштейна (рекурсивно с кешем) \n\
-5. Измерить время \n\
-0. Выход \n\n\
-Выберите пункт: ")
+    print("\n\nМЕНЮ:")
+    print("1. Расстояние Левенштейна")
+    print("2. Расстояние Дамерау-Левенштейна")
+    print("3. Расстояние Дамерау-Левенштейна (рекурсивно)")
+    print("4. Расстояние Дамерау-Левенштейна (рекурсивно с кешем)")
+    print("5. Измерить время")
+    print("0. Выход\n")
+    print("Выберите пункт: ", end="")
 
 
 def menu():
@@ -36,15 +36,18 @@ def menu():
         elif option == 3:
             str1, str2 = input_strings()
             result = damerau_levenshtein_distance_recursive(str1, str2)
-            print("Результат: ", result)
+            print("\nРезультат:", result)
         
         elif option == 4:
             str1, str2 = input_strings()
             result = damerau_levenshtein_distance_recursive_cache(str1, str2)
-            print("Результат: ", result)
+            print("\nРезультат:", result)
             
         elif option == 5:
             test_time_memory()
+
+        elif option == 0:
+            pass
         
         else:
             print("Необходимо ввести цифру от 0 до 5")
