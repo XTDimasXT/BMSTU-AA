bfd184152344a8b908d2378e9d04f18b6a973f74
Pisarenko Dmitriy <dimasxtdimas@yandex.ru>
2023-12-01 17:30:47 +0300

    sort fix and add algorithm schemes

--- lab_03/inc/lst/block_sort.py
+++ lab_03/inc/lst/block_sort.py
@@ -1,27 +1,30 @@
 def block_sort(arr):
-    if len(arr) <= 1:
-        return arr
-    else:
-        mid = len(arr) // 2
-        left = block_sort(arr[:mid])
-        right = block_sort(arr[mid:])
-        
-        return merge(left, right)
+    block_size = 10
+    blocks = []
 
-
-def merge(left, right):
-    result = []
-    i = j = 0
-    
-    while i < len(left) and j < len(right):
-        if left[i] < right[j]:
-            result.append(left[i])
-            i += 1
-        else:
-            result.append(right[j])
+    i = 0
+    while i < len(arr):
+        block = []
+        j = i
+        while j < i + block_size and j < len(arr):
+            block.append(arr[j])
             j += 1
+        block.sort()
+        blocks.append(block)
+        i += block_size
+
+    arr_ind = 0
+    while blocks:
+        min_ind = 0
+        for i in range(1, len(blocks)):
+            if blocks[i][0] < blocks[min_ind][0]:
+                min_ind = i
+        
+        arr[arr_ind] = blocks[min_ind][0]
+        arr_ind += 1
+        blocks[min_ind].pop(0)
+
+        if not blocks[min_ind]:
+            blocks.pop(min_ind)
             
-    result += left[i:]
-    result += right[j:]
-    
-    return result
+    return arr
